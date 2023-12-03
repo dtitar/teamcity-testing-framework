34afdbda2560d483a5ae42f2abc7e9a8d2e1917d
dtitar <dmitry.titar@gmail.com>
2023-12-03 20:45:30 +0300

    add CreateProjectTests

--- src/main/java/com/github/dtitar/teamcity/api/generators/RandomData.java
+++ src/main/java/com/github/dtitar/teamcity/api/generators/RandomData.java
@@ -6,7 +6,19 @@ public class RandomData {
 
     private static final int LENGTH = 10;
 
-    public static String generateString(){
+    public static String generateString() {
         return "test_" + RandomStringUtils.randomAlphabetic(LENGTH);
+    }
+
+    public static String generateString(int length) {
+        return RandomStringUtils.randomAlphabetic(length);
+    }
+
+    public static String generateNumericString() {
+        return RandomStringUtils.randomNumeric(LENGTH);
+    }
+
+    public static String generateNonLatinString() {
+        return "test_" + RandomStringUtils.random(LENGTH, 0x0400, 0x04FF, true, true);
     }
 }
