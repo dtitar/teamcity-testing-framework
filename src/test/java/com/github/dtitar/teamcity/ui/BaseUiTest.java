822836b60c122dccae3050ac7c713c019fdacb4e
dtitar <dmitry.titar@gmail.com>
2024-02-10 17:02:17 +0100

    add logging

--- src/test/java/com/github/dtitar/teamcity/ui/BaseUiTest.java
+++ src/test/java/com/github/dtitar/teamcity/ui/BaseUiTest.java
@@ -2,19 +2,27 @@ package com.github.dtitar.teamcity.ui;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import com.github.dtitar.teamcity.BaseTest;
 import com.github.dtitar.teamcity.api.config.Config;
 import com.github.dtitar.teamcity.api.models.User;
 import com.github.dtitar.teamcity.api.requests.checked.CheckedUser;
 import com.github.dtitar.teamcity.api.spec.Specifications;
 import com.github.dtitar.teamcity.ui.pages.LoginPage;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
 import static java.lang.String.format;
 
 public class BaseUiTest extends BaseTest {
+
+    private static Logger log = LoggerFactory.getLogger(BaseUiTest.class);
+
     @BeforeSuite
     public void setupUiTests() {
         Configuration.browser = Config.getProperty("browser");
@@ -25,8 +33,16 @@ public class BaseUiTest extends BaseTest {
         BrowserSettings.setup(Config.getProperty("browser"));
     }
 
+    @BeforeMethod
+    public void beforeUiTest() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide().
+                includeSelenideSteps(true));
+    }
+
     @AfterMethod
-    public void addVideo(ITestResult result) {
+    public void afterUiTest(ITestResult result) {
+        log.info(format("Test '%s' result is: %s", result.getTestName(), result.getStatus()));
+        log.info(format("Test session id is: %s", Selenide.sessionId()));
         if (result.getStatus() == ITestResult.FAILURE) {
             BrowserSettings.addVideo(String.valueOf(Selenide.sessionId()));
         }
@@ -38,5 +54,4 @@ public class BaseUiTest extends BaseTest {
         new LoginPage().open()
                 .login(user);
     }
-
 }
