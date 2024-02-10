081d88ad9901a9d95fd7f656c3fc8041531670a9
dtitar <dmitry.titar@gmail.com>
2024-02-10 16:31:17 +0100

    add selenoid video

--- src/main/java/com/github/dtitar/teamcity/ui/BrowserSettings.java
+++ src/main/java/com/github/dtitar/teamcity/ui/BrowserSettings.java
@@ -1,13 +1,27 @@
 package com.github.dtitar.teamcity.ui;
 
 import com.codeborne.selenide.Configuration;
+import com.github.dtitar.teamcity.api.config.Config;
+import io.qameta.allure.Allure;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
+import static com.codeborne.selenide.Selenide.sleep;
+import static java.lang.String.format;
+
 public class BrowserSettings {
+
+    private static Logger log = LoggerFactory.getLogger(BrowserSettings.class);
     public static void setup(String browser) {
         Configuration.browser = browser;
 
@@ -36,6 +50,37 @@ public class BrowserSettings {
         Map<String, Object> options = new HashMap<>();
         options.put("enableVNC", true);
         options.put("enableLog", true);
+        options.put("enableVideo", true);
         return options;
+    }
+
+    public static void addVideo(String sessionId) {
+        URL videoUrl = getVideoUrl(sessionId);
+        if (videoUrl != null) {
+            sleep(1000);
+            for (int i = 0; i < 10; i++) {
+                try (InputStream videoInputStream = videoUrl.openStream()) {
+                    Allure.addAttachment("Video", "video/mp4", videoInputStream, "mp4");
+                    break;
+                } catch (FileNotFoundException e) {
+                    sleep(1000);
+                } catch (IOException e) {
+                    log.warn("[ALLURE VIDEO ATTACHMENT ERROR] Cant attach allure video, {}", videoUrl);
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private static URL getVideoUrl(String sessionId) {
+        String videoUrl = format("http://%s/video/%s.mp4", Config.getProperty("host"), sessionId);
+        log.info("Video URL: " + videoUrl);
+        try {
+            return new URL(videoUrl);
+        } catch (MalformedURLException e) {
+            log.warn("[ALLURE VIDEO ATTACHMENT ERROR] Wrong test video url, {}", videoUrl);
+            e.printStackTrace();
+        }
+        return null;
     }
 }
--- src/test/java/com/github/dtitar/teamcity/ui/BaseUiTest.java
+++ src/test/java/com/github/dtitar/teamcity/ui/BaseUiTest.java
@@ -1,12 +1,15 @@
 package com.github.dtitar.teamcity.ui;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import com.github.dtitar.teamcity.BaseTest;
 import com.github.dtitar.teamcity.api.config.Config;
 import com.github.dtitar.teamcity.api.models.User;
 import com.github.dtitar.teamcity.api.requests.checked.CheckedUser;
 import com.github.dtitar.teamcity.api.spec.Specifications;
 import com.github.dtitar.teamcity.ui.pages.LoginPage;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeSuite;
 
 import static java.lang.String.format;
@@ -16,10 +19,17 @@ public class BaseUiTest extends BaseTest {
     public void setupUiTests() {
         Configuration.browser = Config.getProperty("browser");
         Configuration.baseUrl = format("http://%s", Config.getProperty("host"));
-        Configuration.remote=Config.getProperty("remote");
+        Configuration.remote = Config.getProperty("remote");
         Configuration.reportsFolder = "target/surefire-reports";
         Configuration.downloadsFolder = "target/downloads";
         BrowserSettings.setup(Config.getProperty("browser"));
+    }
+
+    @AfterMethod
+    public void addVideo(ITestResult result) {
+        if (result.getStatus() == ITestResult.FAILURE) {
+            BrowserSettings.addVideo(String.valueOf(Selenide.sessionId()));
+        }
     }
 
     public void loginAsUser(User user) {
@@ -28,4 +38,5 @@ public class BaseUiTest extends BaseTest {
         new LoginPage().open()
                 .login(user);
     }
+
 }
