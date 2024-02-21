bc4442ebaeb89d38c0d678ce7d11eb2647d36c0d
dtitar <dmitry.titar@gmail.com>
2024-02-21 08:08:39 +0100

    add JacksonAnnotation JsonIngoreProperties(ignoreUncnown) to models

--- src/main/java/com/github/dtitar/teamcity/api/models/Agent.java
+++ src/main/java/com/github/dtitar/teamcity/api/models/Agent.java
@@ -1,6 +1,6 @@
 package com.github.dtitar.teamcity.api.models;
 
-import io.qameta.allure.internal.shadowed.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
